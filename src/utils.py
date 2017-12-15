f3959236d41a6044db474f7f69e5c3ed79e65c5f
Farit Sadykov <sadykovfarit@gmail.com>
2017-12-15 12:37:55 +0300

    Refine TermColor.
    Surround the escape sequences with '\x01' & '\x02'

--- src/utils.py
+++ src/utils.py
@@ -5,16 +5,43 @@ import string
 
 
 class TermColor:
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    LIGTH_BLUE = '\033[1;34m'
-    PURPLE = '\033[35m'
-    GREY = '\033[1;30m'
-    END = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    """
+    Why we need \001 and \002 escapes, answer provided from the
+    https://bugs.python.org/issue20359:
+
+    Thanks again for the responses and your help. After a bit of research,
+    I discovered the /reasons/ behind needing the \001 and \002 escapes.
+    Thought I'd log the links here for posterity sake:
+      - To color something in on a color capable terminal console you just
+        need to use the "\033[<color code>m" escape sequence.
+        This would be sufficient[1]
+      - However readline messes up the line width calculation because it
+        measures the escape sequences as a characters too. To avoid this you
+        have to wrap the escape sequences within \001 and \002.[2]
+      - On some terminal applications (like the one I am using - terminator[3]),
+        if you add the \001 and \002 escapes to color text which is *not*
+        interpreted by readline, (for instance if you have a single function
+        to color text and you want to use it to color both your sys.ps1 and
+        output text), the \001 and \002 codes will get printed out using a
+        representation (like a unicode 'box'[4]). So, one would have to
+        workaround that in the text coloring function.
+
+        [1] http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+        [2] bugs.python.org/issue17337/ and
+            http://stackoverflow.com/questions/9468435/look-how-to-fix-column-calculation-in-python-readline-if-use-color-prompt
+        [3] http://gnometerminator.blogspot.sg/p/introduction.html
+        [4] http://en.wikipedia.org/wiki/Control_character#Display
+    """
+    RED = '\x01\033[31m\x02'
+    GREEN = '\x01\033[32m\x02'
+    YELLOW = '\x01\033[33m\x02'
+    BLUE = '\x01\033[34m\x02'
+    LIGTH_BLUE = '\x01\033[1;34m\x02'
+    PURPLE = '\x01\033[35m\x02'
+    GREY = '\x01\033[1;30m\x02'
+    END = '\x01\033[0m\x02'
+    BOLD = '\x01\033[1m\x02'
+    UNDERLINE = '\x01\033[4m\x02'
 
     @classmethod
     def coloralize(cls, color, string_to_color):
