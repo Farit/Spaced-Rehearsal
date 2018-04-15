25f12d791172926872bf890535c2e677ef2befbd
Farit Sadykov <sadykovfarit@gmail.com>
2018-04-15 11:39:08 +0300

    Change the scripts working directory.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 
+import os
 import os.path
 import site
 
@@ -7,6 +8,9 @@ import site
 def run():
     project_dir = os.path.dirname(os.path.abspath(__file__))
     site.addsitedir(project_dir)
+    # Change the scripts working directory to the script's own directory,
+    # so that relative paths will work.
+    os.chdir(project_dir)
 
     from src.app import SpacedRehearsal
     spaced_rehearsal = SpacedRehearsal()
