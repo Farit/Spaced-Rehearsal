b8939dc42ba8cee4bdb4585fc0afc65629736f6a
Farit Sadykov <sadykovfarit@gmail.com>
2018-11-30 17:55:14 +0300

    Rewrite app file.

--- src/app.py
+++ src/app.py
@@ -1,36 +1,20 @@
 #!/usr/bin/env python3.6
 
+import logging
 import signal
 import asyncio
 
 
-from src.config import ConfigAdapter
-from src.db_session import DBSession
-from src.create import CreateFlashcard
-from src.play import Play
-from src.search import Search
-from src.alter import AlterFlashcard
-from src.delete import DeleteFlashcard
-from src.utils import TermColor, datetime_now
-from src.base import AsyncIO
 from web_app.app import Application as WebApp
+
+logger = logging.getLogger(__name__)
 
 
 class SpacedRehearsal:
 
-    def __init__(self):
-        self.user = None
-        self.create_flashcard: CreateFlashcard = None
-        self.play_flashcards: Play = None
-        self.search_flashcard: Search = None
-        self.alter_flashcard: AlterFlashcard = None
-        self.delete_flashcard: DeleteFlashcard = None
+    def __init__(self, mediator):
         self.loop = asyncio.get_event_loop()
-        self.async_io = AsyncIO(loop=self.loop)
-        self.config = ConfigAdapter(filename='config.cfg')
-        self.db_session = DBSession(
-            self.config['database'].get('name'), setup_db=True
-        )
+        self.mediator = mediator
         self.web_app = WebApp()
         self.set_signal_handler('sigint')
         self.set_signal_handler('sigterm')
@@ -38,7 +22,8 @@ class SpacedRehearsal:
     def run(self):
         try:
             self.web_app.start()
-            asyncio.ensure_future(self.login(), loop=self.loop)
+            self.mediator.set_loop(self.loop)
+            self.create_task(self.start())
             self.loop.run_forever()
         finally:
             self.loop.close()
@@ -49,198 +34,187 @@ class SpacedRehearsal:
             lambda: None
         )
 
-    async def exit(self, signame='sigterm'):
-        await self.async_io.print(
-            f'Got signal {TermColor.bold(f"{signame}")}',
-            f'{TermColor.red("Exit")}'
+    def create_task(self, coroutine):
+        task = asyncio.ensure_future(
+            self.handle_exception(coroutine),
+            loop=self.loop
         )
-        if self.user:
-            await self.async_io.print(f'Bye {self.user["login"]}!')
-        self.db_session.close()
+
+    async def handle_exception(self, coro):
+        try:
+            await coro
+        except Exception as err:
+            logging.exception(err)
+            print(f'Error: {err}')
+            self.loop.stop()
+
+    async def start(self):
+        await self.mediator.print(
+            f'Web server is running on '
+            f'http://{self.web_app.address}:{self.web_app.port}',
+            bottom_margin=1
+        )
+        self.create_task(self.login())
+
+    async def exit(self, signame='sigterm'):
+        await self.mediator.print(
+            f'Got signal {self.mediator.format_bold(f"{signame}")}',
+            f'{self.mediator.format_red("Exit")}'
+        )
+        await self.mediator.exit()
         self.loop.stop()
 
     async def login(self):
         try:
-            await self.async_io.print(
-                f'Web server is running on '
-                f'http://{self.web_app.address}:{self.web_app.port}'
+            await self.mediator.print(
+                'Please, enter a username.',
+                bold=True
             )
-            await self.async_io.print_formatted_output([
-                TermColor.bold(f'Please, enter a username.'),
-                f'In case if requested user does not exist program will ask '
-                'confirmation to create it. ',
-                f'Pressing {TermColor.red("Ctrl+D")} terminates the program.',
-            ])
-            login_name = await self.async_io.input('Login')
-            user = self.db_session.get_user(login_name)
-            if user is None:
-                action = await self.async_io.input_action(
+            await self.mediator.print(
+                f'In case if requested user does not exist '
+                f'program will ask confirmation to create it. ',
+
+                f'Pressing {self.mediator.format_red("Ctrl+D")} '
+                f'terminates the program.'
+            )
+
+            while True:
+                login_name = await self.mediator.input('Login')
+                login_name = login_name.strip().lower()
+                is_user_logged = await self.mediator.login_user(login_name)
+
+                if is_user_logged:
+                    self.web_app.set_user_id(self.mediator.get_user_id())
+                    self.create_task(self.choose_action())
+                    break
+
+                action = await self.mediator.input_action(
                     action_answers=('y', 'n', 'q'),
                     action_msgs=[
-                        TermColor.bold(
+                        self.mediator.format_bold(
                             f'A user with a login "{login_name}" '
                             f'does not exist.'
                         ),
-                        'Do you want to create it?',
-                        f'Please, enter {TermColor.green("y")} for YES or '
-                        f'{TermColor.red("n")} for NO',
-                        f'Entering {TermColor.red("q")} terminates the program.'
+
+                        f'Do you want to create it?',
+                        f'Please, enter '
+                        f'{self.mediator.format_green("y")} for YES or '
+                        f'{self.mediator.format_red("n")} for NO',
+
+                        f'Entering {self.mediator.format_red("q")} '
+                        f'terminates the program.'
                     ]
                 )
 
+                if action == 'q':
+                    self.create_task(self.exit())
+                    break
+
                 if action == 'y':
-                    method = self.register(login_name)
-                elif action == 'n':
-                    method = self.login()
-                else:
-                    method = self.exit()
-
-                asyncio.ensure_future(method, loop=self.loop)
-
-            else:
-                self.user = self.db_session.get_user(login_name)
-                self.web_app.set_user_id(user_id=self.user['id'])
-                self.create_flashcard = CreateFlashcard(
-                    user_id=self.user['id'], async_io=self.async_io
-                )
-                self.play_flashcards = Play(
-                    user_id=self.user['id'], async_io=self.async_io
-                )
-                self.search_flashcard = Search(
-                    user_id=self.user['id'], async_io=self.async_io
-                )
-                self.alter_flashcard = AlterFlashcard(
-                    user_id=self.user['id'], async_io=self.async_io
-                )
-                self.delete_flashcard = DeleteFlashcard(
-                    user_id=self.user['id'], async_io=self.async_io
-                )
-                asyncio.ensure_future(self.choose_action(), loop=self.loop)
+                    await self.mediator.register_user(login_name)
 
         except EOFError:
-            await self.async_io.print(TermColor.red('Termination!'))
-            asyncio.ensure_future(self.exit(), loop=self.loop)
-
-    async def register(self, login_name):
-        self.db_session.register_user(login_name)
-        await self.async_io.print(f'User {login_name} is registered!')
-        asyncio.ensure_future(self.login(), loop=self.loop)
+            await self.mediator.print('Termination!', red=True)
+            self.create_task(self.exit())
 
     async def choose_action(self):
         try:
-            total_number = self.db_session.count_flashcards(
-                user_id=self.user["id"]
-            )
-            ready_number = self.db_session.count_flashcards(
-                user_id=self.user["id"],
-                review_timestamp=datetime_now()
-            )
-            await self.async_io.print(
+            total_number = await self.mediator.count_total_flashcards()
+            review_number = await self.mediator.count_review_flashcards()
+
+            await self.mediator.print(
                 f'Number of the flashcards: '
-                f'{TermColor.bold(str(total_number))}',
-                f'Number of the flashcards ready to play: '
-                f'{TermColor.bold(str(ready_number))}'
+                f'{self.mediator.format_bold(total_number)}',
+                f'Number of the flashcards ready to review: '
+                f'{self.mediator.format_bold(review_number)}',
+                bottom_margin=1
             )
 
-            action = await self.async_io.input_action(
-                action_answers=('c', 'p', 'a', 's', 'd', 'q'),
+            action = await self.mediator.input_action(
+                action_answers=('r', 'c', 'a', 's', 'd', 'q'),
                 action_msgs=[
-                    f'If you want to {TermColor.yellow("create")} a new '
-                    f'flashcard, please enter {TermColor.yellow("c")}.',
-                    f'If you want to {TermColor.green("play")},'
-                    f' please enter {TermColor.green("p")}.',
-                    f'If you want to {TermColor.light_blue("alter")} '
-                    f'a flashcard, please enter {TermColor.light_blue("a")}.',
-                    f'If you want to {TermColor.light_blue("search")} '
-                    f'a flashcard, please enter {TermColor.light_blue("s")}.',
-                    f'If you want to {TermColor.red("delete")} '
-                    f'a flashcard, please enter {TermColor.red("d")}.',
+                    f'If you want to '
+                    f'{self.mediator.format_green("review")}, '
+                    f'{"please enter".rjust(13)} '
+                    f'{self.mediator.format_green("r")}',
+
+                    f'If you want to '
+                    f'{self.mediator.format_yellow("create")}, '
+                    f'{"please enter".rjust(13)} '
+                    f'{self.mediator.format_yellow("c")}',
+
+                    f'If you want to '
+                    f'{self.mediator.format_light_blue("alter")}, '
+                    f'{"please enter".rjust(14)} '
+                    f'{self.mediator.format_light_blue("a")}',
+
+                    f'If you want to '
+                    f'{self.mediator.format_purple("search")}, '
+                    f'{"please enter".rjust(13)} '
+                    f'{self.mediator.format_purple("s")}',
+
+                    f'If you want to '
+                    f'{self.mediator.format_red("delete")}, '
+                    f'{"please enter".rjust(13)} '
+                    f'{self.mediator.format_red("d")}',
+
                     f'',
-                    f'If you want to {TermColor.red("quit")}, please enter '
-                    f'{TermColor.red("q")}.'
+
+                    f'If you want to '
+                    f'{self.mediator.format_grey("quit")}, '
+                    f'{"please enter".rjust(15)} '
+                    f'{self.mediator.format_grey("q")}'
                 ]
             )
-            if action == 'c':
-                asyncio.ensure_future(self.create(), loop=self.loop)
-            elif action == 'p':
-                asyncio.ensure_future(self.play(), loop=self.loop)
-            elif action == 'a':
-                asyncio.ensure_future(self.alter(), loop=self.loop)
-            elif action == 's':
-                asyncio.ensure_future(self.search(), loop=self.loop)
-            elif action == 'd':
-                asyncio.ensure_future(self.delete(), loop=self.loop)
+            if action == 'q':
+                self.create_task(self.exit())
+                return
+
+            action_names = {
+                'c': 'create',
+                'd': 'delete',
+                'a': 'alter',
+                's': 'search',
+                'r': 'review'
+            }
+
+            self.create_task(
+                self.launch_action(action_name=action_names[action])
+            )
+
+        except EOFError:
+            await self.mediator.print('Termination!', red=True)
+            self.create_task(self.choose_action())
+
+    async def launch_action(self, action_name):
+        try:
+            action_name = action_name.lower()
+
+            if action_name == 'create':
+                await self.mediator.launch_create_action()
+            elif action_name == 'delete':
+                await self.mediator.launch_delete_action()
+            elif action_name == 'alter':
+                await self.mediator.launch_alter_action()
+            elif action_name == 'search':
+                await self.mediator.launch_search_action()
+            elif action_name == 'review':
+                await self.mediator.launch_review_action()
             else:
-                asyncio.ensure_future(self.exit(), loop=self.loop)
+                raise Exception(f'Unknown action name "{action_name}"')
 
         except EOFError:
-            await self.async_io.print(TermColor.red('Termination!'))
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
-
-    async def play(self):
-        try:
-            await self.play_flashcards.play()
-        except EOFError:
-            await self.play_flashcards.print_game_score()
-            await self.async_io.print(TermColor.red('Termination!'))
+            await self.mediator.print(
+                f'{action_name.capitalize()} action termination!',
+                red=True,
+                bottom_margin=1
+            )
         except Exception as err:
-            await self.async_io.print(TermColor.red('Error!'))
+            await self.mediator.print(
+                'Internal Error!',
+                red=True,
+                bottom_margin=1
+            )
             raise err
         finally:
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
-
-    async def create(self):
-        try:
-            await self.create_flashcard.add()
-        except EOFError:
-            self.create_flashcard.popleft_previous_sources()
-            await self.async_io.print(TermColor.red('Termination!'))
-
-        except Exception as err:
-            self.create_flashcard.popleft_previous_sources()
-            await self.async_io.print(TermColor.red('Error!'))
-            raise err
-
-        finally:
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
-
-    async def search(self):
-        try:
-            await self.search_flashcard.launch_search()
-        except EOFError:
-            await self.async_io.print(TermColor.red('Termination!'))
-
-        except Exception as err:
-            await self.async_io.print(TermColor.red('Error!'))
-            raise err
-
-        finally:
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
-
-    async def alter(self):
-        try:
-            await self.alter_flashcard.alter()
-        except EOFError:
-            await self.async_io.print(TermColor.red('Termination!'))
-
-        except Exception as err:
-            await self.async_io.print(TermColor.red('Error!'))
-            raise err
-
-        finally:
-            await self.create_flashcard.update_source_tags()
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
-
-    async def delete(self):
-        try:
-            await self.delete_flashcard.delete()
-        except EOFError:
-            await self.async_io.print(TermColor.red('Termination!'))
-
-        except Exception as err:
-            await self.async_io.print(TermColor.red('Error!'))
-            raise err
-
-        finally:
-            await self.create_flashcard.update_source_tags()
-            asyncio.ensure_future(self.choose_action(), loop=self.loop)
+            self.create_task(self.choose_action())
