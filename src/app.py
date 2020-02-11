a272ab2c346e1ebf5d088d1574d6a5f009ba0adf
Farit Sadykov <sadykovfarit@gmail.com>
2020-02-11 06:55:54 +0300

    Refine choose_action method.

--- src/app.py
+++ src/app.py
@@ -123,17 +123,21 @@ class SpacedRehearsal:
     async def choose_action(self):
         try:
             total_number = await self.mediator.count_total_flashcards()
-            review_number = await self.mediator.count_review_flashcards()
+            total_text = total_number['text']
+            total_audio = total_number['audio']
 
+            review_number = await self.mediator.count_review_flashcards()
+            review_text = review_number['text']
+            review_audio = review_number['audio']
+
+            table = [
+                ["", "Text", "Audio"],
+                ["Total flashcards", total_text, total_audio],
+                ["Ready to review", review_text, review_audio],
+            ]
+            table = self.format_table(table)
             await self.mediator.print(
-                f'Number of the text flashcards: '
-                f'{self.mediator.format_bold(total_number["text"])}',
-                f'Number of the audio flashcards: '
-                f'{self.mediator.format_bold(total_number["audio"])}',
-                f'Number of the text flashcards ready to review: '
-                f'{self.mediator.format_bold(review_number["text"])}',
-                f'Number of the audio flashcards ready to review: '
-                f'{self.mediator.format_bold(review_number["audio"])}',
+                *table,
                 bottom_margin=1
             )
 
@@ -225,3 +229,31 @@ class SpacedRehearsal:
             raise err
         finally:
             self.create_task(self.choose_action())
+
+    @staticmethod
+    def format_table(table):
+        formatted_table = []
+
+        num_of_rows = len(table)
+        num_of_cols = len(table[0])
+        max_cols_width = []
+        right_margin = 3
+
+        for j in range(num_of_cols):
+            max_column_width = float('-inf')
+            for i in range(num_of_rows):
+                current_column_width = len(str(table[i][j]))
+                if current_column_width > max_column_width:
+                    max_column_width = current_column_width
+            max_cols_width.append(max_column_width + right_margin)
+
+        row_format = ""
+        for ind, max_width in enumerate(max_cols_width):
+            if ind == 0:
+                row_format += f'{{:<{max_width}}}'
+            else:
+                row_format += f'{{:>{max_width}}}'
+
+        for row in table:
+            formatted_table.append(row_format.format(*row))
+        return formatted_table
